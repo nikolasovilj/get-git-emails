ded1e7ac4b25e1eeedbdf33e7a5ca9ca47e7e2cd
Nikola Sovilj <23420051+nikolasovilj@users.noreply.github.com>
2026-06-24 11:47:42 +0200

    last modifications

--- get-git-emails.py
+++ get-git-emails.py
@@ -1,64 +1,57 @@
 #!/usr/bin/env python3
 import os, sys
 import requests as r
+from utils import get_next_link
+import logging
 
-def get_next_link(link_header):
-    # Split the Link header into individual link components
-    links = link_header.split(", ")
-    
-    # Loop through the parts and find the "next" link
-    for link in links:
-        # Check if the 'rel="next"' is present
-        if 'rel="next"' in link:
-            # Extract the URL from the angle brackets
-            next_link = link[link.find("<") + 1:link.find(">")]
-            return next_link
-    # Return None if no "next" link is found (i.e., end of pagination)
-    raise Exception
+
+logging.basicConfig(level=logging.INFO, format="{message}", style="{")
 
 api_key = os.getenv("gh_api_key")
 if not api_key:
-    print("make sure you set gh_api_key env variable!")
+    logging.info("make sure you set gh_api_key env variable!")
     sys.exit(1)
 
+username = input("Enter GitHub username: ")
+
 headers = {'Authorization': f'token {api_key}'}
-username = input("Enter your username: ")
 url = f"https://api.github.com/users/{username}/repos?per_page=10&page=1"
+
 repos = []
+
 while True:
     res = r.get(url, headers=headers)
-    #print(res.status_code)
     for repo in res.json():
-        print(repo['name'])
+        logging.info(repo['name'])
         repos.append(repo['name'])
-    print(res.headers['Link'])
+    logging.info(res.headers['Link'])
     try:
         url = get_next_link(res.headers['Link'])
     except Exception as e:
-        print("End of paginated repository discovery!")
-        print(e)
+        logging.error("End of paginated repository discovery!")
+        logging.error(e)
         break
 
-print(f"found {len(repos)} repos!")
-print(repos)
+logging.info(f"found {len(repos)} repos!")
+logging.info(repos)
 
 emails = set()
 for repo in repos:
     url = f"https://api.github.com/repos/{username}/{repo}/commits"
-    print(f"getting: {url}")
+    logging.info(f"getting: {url}")
     res = r.get(url, headers=headers)
-    #print(res.json())
-    #print(type(res.json()))
+    #logging.info(res.json())
+    #logging.info(type(res.json()))
     for commit in res.json():
         try:
-            print(commit['commit']['message'])
-            print(commit['commit']['author']['email'])
+            #logging.info(commit['commit']['message'])
+            #logging.info(commit['commit']['author']['email'])
             emails.add(commit['commit']['author']['email'])
-            print(commit['commit']['committer']['email'])
+            #logging.info(commit['commit']['committer']['email'])
             emails.add(commit['commit']['committer']['email'])
         except:
             pass
 
-print("emails found:")
+logging.info("\nemails found:")
 for email in emails:
-    print(email)
+    if email: print(email)
--- utils.py
+++ utils.py
@@ -1,21 +1,8 @@
-def add_url_scheme(url):
-    if "https://" in url: return url
-    else:
-        if "http://" in url: return url.replace("http://", "https://")
-        else: return "https://" + url
-
-def get_mode(url):
-    # returns variants based on which we will later crawl github
-    # should return either 'user_profile', 'organization', 'repo' or unsupported 
-    #
-    # pogledaj kako se radi docstring
-    # returns tuple
-    url_keywords = [
-            "/marketplace",
-            "/actions",
-
-        ]
-    if any(url_keyword in url for url_keyword in url_keywords):
-        return "unsupported"
-
-
+def get_next_link(link_header):
+    links = link_header.split(", ")
+    
+    for link in links:
+        if 'rel="next"' in link:
+            next_link = link[link.find("<") + 1:link.find(">")]
+            return next_link
+    raise Exception
